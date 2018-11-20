363278087e92123555a9f597b78715c9931e1864
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-11-20 14:51:08 -0600

    building tree crawler to user recursive method call

--- binary_tree.rb
+++ binary_tree.rb
@@ -6,6 +6,9 @@
 
 #sample data set --> [47, 3, 32, 43, 19, 6, 18, 36, 13, 0]
 
+# x = 10.times.map{ rand(0...30) } 
+# p x
+
 class Node 
   attr_accessor :value, :parent_node,:left_node, :right_node 
   def initialize(value = nil, parent_node = nil, left_node = nil, right_node = nil)
@@ -16,17 +19,43 @@ class Node
     end
 
     def node_placement(next_value,parent_node)
-      if next_value > parent_node.value
-        parent_node.right_node = Node.new(next_value)
-      elsif next_value < parent_node.value
-        parent_node.left_node = Node.new(next_value)  
+      current_node = parent_node
+      if next_value < current_node.value
+        if current_node.left_node == nil
+          current_node.left_node = next_value
+        else 
+          node_placement(next_value,current_node)
+        end 
+
+      elsif next_value >= current_node.value
+        if current_node.right_node == nil
+          current_node.right_node = next_value
+        else 
+          node_placement(next_value,current_node)
+        end 
+
       end
     end
 
 end
 
-#[47, 3, 32, 43, 19, 6, 18, 36, 13, 0]
+#[47, 3, 52, 43, 19, 6, 18, 36, 13, 0]
 new_tree_root = Node.new(47,nil) 
 new_tree_root.node_placement(3,new_tree_root)
+new_tree_root.node_placement(52,new_tree_root)
 
+
+puts ' - - - - - -'
 p new_tree_root
+
+
+#<Node: @value = 47, 
+#@parent_node  = nil, 
+#@left_node    = Node:@value=3, 
+                   #@parent_node = 47, 
+                   #@left_node   = nil, 
+                   #@right_node  = Node @value=32, 
+                                    # @parent_node = 3, 
+                                    # @left_node   = nil, 
+                                    # @right_node  = nil>>, 
+#@right_node = nil>
