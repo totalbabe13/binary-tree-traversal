d70f7c3cc548dc04f064cf0da2eb5702738bb951
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-12-03 16:56:33 -0600

    level order search COMPLETED

--- binary_tree.rb
+++ binary_tree.rb
@@ -8,6 +8,7 @@ class Node
     @value = value
   end  
 end
+
 # - - - - - - - - - - - - - - - - -
 def build_tree(data)
  root = Node.new(data[0])
@@ -18,7 +19,8 @@ def build_tree(data)
    data.shift
   end
   root
-end#end of build_tree
+end
+
 # - - - - - - - - - - - - - - - - -
 def insert(new_node,root)
   if new_node.value < root.value
@@ -38,68 +40,38 @@ def insert(new_node,root)
 end
 # - - - - - - - - - - - - - - - - -
 def breadth_first_search(tree,target)
-#NOTE figure out what to with nil v
   root = tree
-  l = root.left
-  r = root.right
   queue = [] 
-  all_values = []
+  visited_nodes = []
   
-  if verify_leaf_node(root) == false
-    que.push(root.value)
-    que.push(root.left.value)
-    que.push(root.right.value)
-  end
+  queue.push(root) 
+  visited_nodes.push(queue[0].value)
   
-  if verify_leaf_node(l) == false
-    if l.left != nil
-      que.push(l.left.value)
-    end  
-    if l.right != nil
-      que.push(l.right.value)
-    end 
+  while queue.empty? == false
+    if root.left != nil
+      queue.push(root.left)
+      visited_nodes.push(root.left.value)
+    end
+    
+    if root.right != nil
+      queue.push(root.right)
+      visited_nodes.push(root.right.value)
+    end
+    root = queue.shift
+    
+    if root.value == target
+      return root
+    end    
   end 
-
-  if verify_leaf_node(r) == false
-    if r.left != nil
-      que.push(r.left.value)
-    end  
-    if r.right != nil
-      que.push(r.right.value)
-    end 
-  end 
-
-
-  
-  p que
-  # UNTIL right node is a leaf??
-  # BOTH left AND right links are both NIL on a right node???
-  
+  nil
 end
+
 # - - - - - - - - - - - - - - - - -
-def verify_leaf_node(node)
-  if node.left.value == nil && node.right.value == nil
-     true
-  else
-     false
-  end     
-end
+# - R - U - N - N - E - R 
 # - - - - - - - - - - - - - - - - -
 x = [5, 9, 2, 7, 1, 3, 6, 8, 4 ]
 test_tree = build_tree(x)
-puts '- - - -'
-breadth_first_search(test_tree,9)
+breadth_first_search(test_tree,7)
 
 
 
-
-
-  # p root.value                  #5
-  # p root.left.value             #2
-  # p root.left.left.value        #1
-  # p root.left.right.value       #3
-  # p root.left.right.right.value #4 
-  # p root.right.value            #9
-  # p root.right.left.value       #7
-  # p root.right.left.left.value  #6
-  # p root.right.left.right.value #8
